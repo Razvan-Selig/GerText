6ebd97e76e9c58e5e61250259fb3260048aba4e5
Selig Razvan <razvanselig02@gmail.com>
2025-04-13 21:09:47 +0200

    Added css media queries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import "../styles/Home.css"
 import hero from "../assets/hero-2.png"
+import pers_1 from "../assets/pers-1.jpg"
+import pers_2 from "../assets/pers-2.jpg"
+import pers_3 from "../assets/pers-3.jpg"
 
 function Home() {
   return (
@@ -36,7 +39,28 @@ function Home() {
 
         </div>
         <div className="testimonials">
-            
+
+            <div className="testimonial testimonial-1">
+                <img src={pers_1} alt="" />
+                <p className="name">Sophie L.</p>
+                <p className="level">B1-B2</p>
+                <p className="text">"As a doctor I need technical German. This website helped me read medical articles!"</p>
+            </div>
+
+            <div className="testimonial testimonial-2">
+                <img src={pers_2} alt="" />
+                <p className="name">Yuki S.</p>
+                <p className="level">A2-B1</p>
+                <p className="text">"Not just language – I also learned German culture through texts about traditions."</p>
+            </div>
+
+            <div className="testimonial testimonial-3">
+                <img src={pers_3} alt="" />
+                <p className="name">Marco T</p>
+                <p className="level">B2-C1</p>
+                <p className="text">"Thanks to the business texts, I can now negotiate with German clients easily."</p>
+            </div>
+
         </div>
       </main>
     </>
